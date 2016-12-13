d2cae766c5f729ce1969e127b6c67fca95a4a2c8
xszhaobo <azhaobo1991@163.com>
2016-12-13 23:14:26 +0800

    Java I/O系统，视图缓冲器

---
+++ src/main/java/nio/ViewBuffers.java
@@ -0,0 +1,31 @@
+package nio;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+
+/**
+ * author:xszhaobo
+ * <p/>
+ * date:2016/12/13
+ * <p/>
+ * package_name:nio
+ * <p/>
+ * project: MyPractice
+ */
+public class ViewBuffers {
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[]{0,0,0,0,0,0,0,'a'});
+        buffer.rewind();
+        System.out.print("Byte Buffer ");
+        while (buffer.hasRemaining()) {
+            System.out.printf(buffer.position() + "->" + buffer.get() + ",");
+        }
+        System.out.println("");
+
+        CharBuffer charBuffer = ((ByteBuffer) buffer.rewind()).asCharBuffer();
+        System.out.printf("Char Buffer ");
+        while (charBuffer.hasRemaining()) {
+            System.out.printf(charBuffer.position() + "->" + charBuffer.get() + ",");
+        }
+    }
+}
