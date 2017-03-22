650982d2601608da6009e2b850a0ae62d41be5e5
xszhaobo <azhaobo1991@163.com>
2017-03-22 23:17:29 +0800

    访问控制修饰符的测试

---
+++ src/main/java/Cake.java
@@ -0,0 +1,15 @@
+/**
+ * author:xszhaobo
+ * <p/>
+ * date:2017/3/22
+ * <p/>
+ * package_name:PACKAGE_NAME
+ * <p/>
+ * project: MyPractice
+ */
+class Cake {
+    public static void main(String[] args) {
+        Pie pie = new Pie();
+        pie.f();
+    }
+}
---
+++ src/main/java/Pie.java
@@ -0,0 +1,15 @@
+/**
+ * author:xszhaobo
+ * <p/>
+ * date:2017/3/22
+ * <p/>
+ * package_name:PACKAGE_NAME
+ * <p/>
+ * project: MyPractice
+ */
+class Pie {
+
+    void f() {
+        System.out.println("Pie.f()");
+    }
+}
