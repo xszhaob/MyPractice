657552ed52ab48b1b5a552a1d6d0674e3ec18e19
zhaobo <azhaobo1991@163.com>
2017-08-07 22:56:31 +0800

    实验class.isAssignableFrom方法

---
+++ src/main/java/bo/zhao/practice/lang/ClassTest.java
@@ -0,0 +1,10 @@
+package bo.zhao.practice.lang;
+
+/**
+ * Created by zhaobo on 2017/8/7.
+ */
+public class ClassTest {
+    public static void main(String[] args) {
+        System.out.println(ClassTest.class.isAssignableFrom(Object.class));
+    }
+}
