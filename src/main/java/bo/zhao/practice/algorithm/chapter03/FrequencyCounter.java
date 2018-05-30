1b03400e527e8f3c9a178248236f3315f80a866b
xszhaobo <azhaobo1991@163.com>
2018-05-30 23:40:26 +0800

    查找算法性能测试

--- src/main/java/bo/zhao/practice/algorithm/chapter03/FrequencyCounter.java
+++ src/main/java/bo/zhao/practice/algorithm/chapter03/FrequencyCounter.java
@@ -1,45 +1,82 @@
 package bo.zhao.practice.algorithm.chapter03;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by zhaobo on 2018/5/17.
  */
 public class FrequencyCounter {
-    public static void main(String[] args) {
-        int minLen = 0;
+   private static final Set<Character> SET = new HashSet<>();
 
-        List<String> tuples = STBaseTest.mockStringList(1000);
+    static {
+        SET.add(',');
+        SET.add('\"');
+        SET.add(';');
+        SET.add('.');
+        SET.add('?');
+        SET.add(')');
+        SET.add('(');
+        SET.add('!');
+    }
+    public static void main(String[] args) throws Exception {
+        test(8, new BST<>());
+    }
 
-        ST<String, Integer> st = new SequentialSearchST<>();
-        int putCount = 0;
-        int getCount = 0;
-        for (String str : tuples) {
-            if (str.length() < minLen) {
-                continue;
+
+    private static void test(int minLen, ST<String, Integer> st) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\zhaobo\\Desktop\\tale.txt"))) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                if (line.trim().length() == 0) {
+                    line = reader.readLine();
+                    continue;
+                }
+                String[] split = clear(line.trim()).split("\\s+");
+                if (split.length == 0) {
+                    line = reader.readLine();
+                    continue;
+                }
+                for (String s : split) {
+                    if (s.length() < minLen) {
+                        continue;
+                    }
+                    if (!st.contains(s)) {
+                        st.put(s, 1);
+                    } else {
+                        st.put(s, st.get(s) + 1);
+                    }
+                }
+
+                line = reader.readLine();
             }
-            if (!st.contains(str)) {
-                st.put(str, 1);
-            } else {
-                getCount++;
-                st.put(str, st.get(str) + 1);
-            }
-            putCount++;
         }
 
-//        String max = "";
-//
-//        st.put(max, 0);
+        String max = "";
+        st.put(max, 0);
+        for (String s : st.keys()) {
+            if (st.get(s) > st.get(max)) {
+                max = s;
+            }
+        }
 
-//        for (String s : st.keys()) {
-//            if (st.get(s) > st.get(max)) {
-//                max = s;
-//            }
-//        }
+        System.out.println(max + " " + st.get(max));
+    }
 
-        System.out.println("keys.size()=" + st.size());
-//        System.out.println(max + " " + st.get(max));
-        System.out.println("put count = " + putCount);
-        System.out.println("get count = " + getCount);
+    private static String clear(String str) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (SET.contains(c)) {
+                sb.append(" ");
+                continue;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
