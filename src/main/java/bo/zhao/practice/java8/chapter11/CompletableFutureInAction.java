a2aa8d11b3eaff2f5261e7f1bab7d2296203c428
zhaobo <azhaobo1991@163.com>
2018-07-14 01:23:25 +0800

    Java8，CompletableFutures的实践

--- src/main/java/bo/zhao/practice/java8/chapter11/CompletableFutureInAction.java
+++ src/main/java/bo/zhao/practice/java8/chapter11/CompletableFutureInAction.java
@@ -1,10 +1,12 @@
 package bo.zhao.practice.java8.chapter11;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -17,24 +19,47 @@ import java.util.stream.Collectors;
  */
 public class CompletableFutureInAction {
 
+    private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(createShopList().size(), r -> {
+        Thread t = new Thread(r);
+        t.setDaemon(true);
+        return t;
+    });
+
     public static void main(String[] args) {
         String product = "MyPhone27";
 
-        findPricesInAction(() -> findPrices(product));
+//        findPricesInAction(() -> findPrices(product));
 
-        findPricesInAction(() -> findPricesInParallelStream(product));
+//        findPricesInAction(() -> findPricesInParallelStream(product));
 
-        findPricesInAction(() -> findPricesFuture(product));
+//        findPricesInAction(() -> findPricesFuture(product));
 
-        findPricesInAction(() -> findDiscountPrices(product));
+//        findPricesInAction(() -> findDiscountPrices(product));
 
-        findPricesInAction(() -> findDiscountPricesFuture(product));
+//        findPricesInAction(() -> findDiscountPricesFuture(product));
+
+//        findPricesInAction(() -> findPricesInSerialFuture(product));
+
+        findPricesInActionTemplate(CompletableFutureInAction::findPricesStream, product);
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 
 
+    private static <T> void findPricesInVoidAction(Consumer<T> consumer, T t) {
+        findPricesInActionTemplate(consumer, t);
+    }
+
     private static void findPricesInAction(Supplier<List<String>> supplier) {
+        findPricesInActionTemplate(System.out::println, supplier.get());
+    }
+
+    private static <T> void findPricesInActionTemplate(Consumer<T> consumer, T t) {
         long start = System.currentTimeMillis();
-        System.out.println(supplier.get());
+        consumer.accept(t);
         System.out.println("Done in " + (System.currentTimeMillis() - start) + " millis time!");
     }
 
@@ -66,16 +91,11 @@ public class CompletableFutureInAction {
 
     private static List<String> findPricesFuture(String product) {
         List<Shop> shops = createShopList();
-        ExecutorService executor = Executors.newFixedThreadPool(shops.size(), r -> {
-            Thread t = new Thread(r);
-            t.setDaemon(true);
-            return t;
-        });
 
         List<CompletableFuture<String>> future = shops.stream().map((
                 shop -> CompletableFuture.supplyAsync(
                         () -> shop.getShopName() + " price is " + shop.getPrice(product),
-                        executor)
+                        EXECUTOR)
         )).collect(Collectors.toList());
 
         return future.stream().map(CompletableFuture::join).collect(Collectors.toList());
@@ -91,16 +111,56 @@ public class CompletableFutureInAction {
 
     private static List<String> findDiscountPricesFuture(String product) {
         List<Shop> shops = createShopList();
-        ExecutorService executor = Executors.newFixedThreadPool(shops.size(), r -> {
-            Thread t = new Thread(r);
-            t.setDaemon(true);
-            return t;
-        });
         List<CompletableFuture<String>> priceFutures = createShopList().stream()
-                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPrice(product), executor))
+                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPrice(product), EXECUTOR))
                 .map(future -> future.thenApply(Quote::parse))
-                .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(() -> Discount.applyDiscount(quote), executor)))
+                .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(() -> Discount.applyDiscount(quote), EXECUTOR)))
                 .collect(Collectors.toList());
         return priceFutures.stream().map(CompletableFuture::join).collect(Collectors.toList());
+    }
+
+    private static List<String> findPricesInSerialFuture(String product) {
+        List<Shop> shops = createShopList();
+
+        List<String> prices = new ArrayList<>(6);
+        CompletableFuture<String> future0 = CompletableFuture.supplyAsync(() -> shops.get(0).getPrice(product), EXECUTOR);
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> shops.get(1).getPrice(product), EXECUTOR);
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> shops.get(2).getPrice(product), EXECUTOR);
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> shops.get(3).getPrice(product), EXECUTOR);
+        CompletableFuture<String> future4 = CompletableFuture.supplyAsync(() -> shops.get(4).getPrice(product), EXECUTOR);
+        CompletableFuture<String> future5 = CompletableFuture.supplyAsync(() -> shops.get(5).getPrice(product), EXECUTOR);
+        CompletableFuture<String> future6 = CompletableFuture.supplyAsync(() -> shops.get(6).getPrice(product), EXECUTOR);
+
+        try {
+            prices.add(future0.get());
+            prices.add(future1.get());
+            prices.add(future2.get());
+            prices.add(future3.get());
+            prices.add(future4.get());
+            prices.add(future5.get());
+            prices.add(future6.get());
+            return prices;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void findPricesStream(String product) {
+        List<CompletableFuture<String>> futureList = createShopList().stream()
+                .map(shop ->
+                        CompletableFuture.supplyAsync(() -> shop.getPriceInRandomDelay(product), EXECUTOR))
+                .map(future -> future.thenApply(Quote::parse))
+                .map(future -> future.thenCompose((quote) ->
+                        CompletableFuture.supplyAsync(() ->
+                                Discount.applyDiscount(quote), EXECUTOR)))
+                .collect(Collectors.toList());
+
+        long start = System.currentTimeMillis();
+        CompletableFuture[] futures = futureList.stream()
+                .map(future -> future.thenAccept((price) ->
+                        System.out.println(
+                                price + "(done in " + (System.currentTimeMillis() - start) + ")")))
+                .toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(futures).join();
     }
 }
--- src/main/java/bo/zhao/practice/java8/chapter11/Shop.java
+++ src/main/java/bo/zhao/practice/java8/chapter11/Shop.java
@@ -2,6 +2,7 @@ package bo.zhao.practice.java8.chapter11;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 /**
  * 文件描述：
@@ -44,19 +45,44 @@ public class Shop {
     }
 
     public String getPrice(String product) {
-        double price = calculatePrice(product);
+        return formatByDiscount(calculatePriceByFixed(product));
+    }
+
+    public String getPriceInRandomDelay(String product) {
+        return formatByDiscount(calculatePriceByRandom(product));
+    }
+
+    private String formatByDiscount(double price) {
         Discount.Code code = Discount.Code.values()[random.nextInt(Discount.Code.values().length)];
         return String.format("%s:%.2f:%s", getShopName(), price, code);
     }
 
-    private double calculatePrice(String product) {
+    private double calculatePriceByFixed(String product) {
         delay();
+        return calculatePrice(product);
+    }
+
+    private double calculatePriceByRandom(String product) {
+        randomDelay();
+        return calculatePrice(product);
+    }
+
+    private double calculatePrice(String product) {
         return random.nextDouble() * product.charAt(0) + product.charAt(1);
     }
 
     private static void delay() {
         try {
             Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void randomDelay() {
+        try {
+            int delay = 500 + random.nextInt(2000);
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
